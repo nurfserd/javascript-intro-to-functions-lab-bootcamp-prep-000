15095a351bfdddf77ac751f659b508224bb35818
Steve Anderson <nurfserd@gmail.com>
2017-11-10 02:28:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase === lowercase) {
     return("I can't hear you!")
-  } else if (string.toUpperCase === uppercase {
+  } else if (string.toUpperCase === uppercase) {
     return ("YES INDEED!")
   } else if (string == "I love you, Grandma.") {
     return ("I love you, too.")
