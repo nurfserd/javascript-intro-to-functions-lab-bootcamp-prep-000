cff8da97123bf7c7d35c38271b6b59ff942cd7dc
Steve Anderson <nurfserd@gmail.com>
2017-11-10 02:33:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() == true) {
+  if (string.toLowerCase() === true) {
     return("I can't hear you!")
-  } else if (string.toUpperCase() == true) {
+  } else if (string.toUpperCase() === true) {
     return ("YES INDEED!")
   } else if (string == "I love you, Grandma.") {
     return ("I love you, too.")
